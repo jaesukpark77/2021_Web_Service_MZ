f4d50af0ccf2193ca4b005c4224e3f07c06ed777
jaesukpark77 <jaesukpark77@naver.com>
2021-11-24 18:31:11 +0900

    js 추가

---
+++ js/mypage.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#headerWrap #titleBox #title').click(function(){
+        window.location.href="../html/home.html";
+    });
+    $('#headerWrap #titleBox #mypage').click(function(){
+        window.location.href="../html/mypage.html";
+    });
+    $('#headerWrap #titleBox #login').click(function(){
+        window.location.href="../html/login.html";
+    });
+    $('#headerWrap #titleBox #join').click(function(){
+        window.location.href="../html/join.html";
+    });
+});
